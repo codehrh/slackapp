7fa191643e13977db39643002c2de8c50e8451d3
Shannelle <shannellechua@gmail.com>
2024-08-29 20:36:54 +0800

    Styled login

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import { API_URL } from './constants/Constants';
 import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faKey , faUserPlus} from '@fortawesome/free-solid-svg-icons'
 import Dashboard from './pages/Dashboard';
 import SignUp from './pages/SignUp';
 
@@ -11,10 +13,10 @@ import SignUp from './pages/SignUp';
 // import AddUsers from "./components/AddUsers";
 
 function App() {
-  const [ isLoggedIn, setIsLoggedIn ] = useState(false)
-  const [ email, setEmail ] = useState();
-  const [ password, setPassword ] = useState();
-  const [ user, setUser ] = useState(() => 
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [user, setUser] = useState(() =>
     JSON.parse(localStorage.getItem("user"))
   );
 
@@ -22,82 +24,87 @@ function App() {
 
   const [showSignup, setShowSignup] = useState(false);
 
-  useEffect(()=>{
-    if(user){
+  useEffect(() => {
+    if (user) {
       setIsLoggedIn(true)
       localStorage.setItem("user", JSON.stringify(user))
     }
-  },[user])
+  }, [user])
 
-    async function handleSubmit(event) {
-      event.preventDefault();
-      if (!email || !password) {
-        return alert ("Invalid Credentials");
+  async function handleSubmit(event) {
+    event.preventDefault();
+    if (!email || !password) {
+      return alert("Invalid Credentials");
+    }
+    try {
+      const loginCredentials = {
+        email, password
       }
-      try {
-        const loginCredentials = {
-          email, password
-        }
-        const response = await axios.post(
-          `${API_URL}/auth/sign_in`, 
-          loginCredentials)
-        const {data, headers} = response;
+      const response = await axios.post(
+        `${API_URL}/auth/sign_in`,
+        loginCredentials)
+      const { data, headers } = response;
 
-        if (data && headers) {
-          const accessToken = headers["access-token"];
-          const expiry = headers["expiry"]
-          const client = headers["client"]
-          const uid = headers["uid"]
+      if (data && headers) {
+        const accessToken = headers["access-token"];
+        const expiry = headers["expiry"]
+        const client = headers["client"]
+        const uid = headers["uid"]
 
-          setUser({
-            accessToken, 
-            expiry,
-            client,
-            uid,
-            id: data.data.id,
-          })
-          
-          setIsLoggedIn(true)
-        }
-      } catch(error) {
-        if(error.response.data.errors) {
-          return alert ("Invalid Credentials")
-        }
+        setUser({
+          accessToken,
+          expiry,
+          client,
+          uid,
+          id: data.data.id,
+        })
+
+        setIsLoggedIn(true)
+      }
+    } catch (error) {
+      if (error.response.data.errors) {
+        return alert("Invalid Credentials")
       }
     }
+  }
 
-    function toggleSignup() {
-      showSignup ? setShowSignup(false) : setShowSignup(true);
-    }
+  function toggleSignup() {
+    showSignup ? setShowSignup(false) : setShowSignup(true);
+  }
 
   return (
     <div className="App">
-      {!isLoggedIn && (
-        <form onSubmit={handleSubmit}>
-          <label>Email:</label>
-          <input 
-            type="email"
-            onChange={(event) => setEmail(event.target.value)}
-          ></input>
-          <label>Password:</label>
-          <input 
-            type="password"
-            onChange={(event) => setPassword(event.target.value)}
-          ></input>
-          <button type="submit">Login</button>
-          <br/>
-          <button type="button" onClick={toggleSignup}>Sign Up</button>
-        </form>
-      )}
-      {showSignup && (
-        <SignUp>
-          setShowSignup={setShowSignup}
-          setIsLoggedIn={setIsLoggedIn}
-        </SignUp>
-      )}
-      {isLoggedIn && (
-        <Dashboard setIsLoggedIn={setIsLoggedIn} user={user}></Dashboard>
-      )}
+      <h1>This is a h1/Slack</h1>
+      <div className="login">
+        {!isLoggedIn && (
+          <form onSubmit={handleSubmit}>
+            <label>Email</label>
+            <input
+              type="email"
+              onChange={(event) => setEmail(event.target.value)}
+            ></input><br />
+            <label>Password</label>
+            <input
+              type="password"
+              onChange={(event) => setPassword(event.target.value)}
+            ></input>
+            <div className='two-buttons'>
+              <button type="submit"><FontAwesomeIcon icon={faKey} className='icon'/>Login</button>
+              <br />
+              <button type="button" className="secondary-button" onClick={toggleSignup}><FontAwesomeIcon icon={faUserPlus} className='icon'/>Sign Up</button>
+            </div>
+          </form>
+        )}
+        {showSignup && (
+          <SignUp>
+            setShowSignup={setShowSignup}
+            setIsLoggedIn={setIsLoggedIn}
+          </SignUp>
+        )}
+        {isLoggedIn && (
+          <Dashboard setIsLoggedIn={setIsLoggedIn} user={user}></Dashboard>
+        )}
+      </div>
     </div>
   );
 }
