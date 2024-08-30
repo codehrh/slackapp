dedd0980765c4596c64f928c73ec3850e056f124
JakeN18 <jkdelvalle18@yahoo.com.ph>
2024-08-30 22:33:44 +0800

    merging

--- src/App.js
+++ src/App.js
@@ -3,31 +3,9 @@ import { useState, useEffect } from 'react';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-<<<<<<< HEAD
-export default function App() {
-  const [user, setUser] = useState(null);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-const router = createBrowserRouter([
-
-  {
-    path: "/",
-    element: <Login/>,
-  },
-  {
-    path: "/dashboard",
-    element: <Dashboard/>,
-  },
-
-])
-
-export default function App() {
-=======
-
 function App() {
   const [user, setUser] = useState(null);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-
   useEffect(() => {
     const storedUser = localStorage.getItem('user');
     if (storedUser) {
@@ -35,7 +13,6 @@ function App() {
       setIsLoggedIn(true);
     }
   }, []);
-
   const updateUser = (userData) => {
     setUser(userData);
     setIsLoggedIn(!!userData);
@@ -45,15 +22,14 @@ function App() {
       localStorage.removeItem('user');
     }
   };
-
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Login setUser={updateUser} setIsLoggedIn={setIsLoggedIn}/>, 
+      element: <Login setUser={updateUser} setIsLoggedIn={setIsLoggedIn}/>,
     },
     {
       path: "/dashboard",
-      element: <Dashboard user={user} setUser={updateUser} setIsLoggedIn={setIsLoggedIn}/>, 
+      element: <Dashboard user={user} setUser={updateUser} setIsLoggedIn={setIsLoggedIn}/>,
     },
   ]);
   return (
@@ -61,10 +37,5 @@ function App() {
       <RouterProvider router={router} />
     </div>
   );
-<<<<<<< HEAD
 }
-=======
-}
-
 export default App;
->>>>>>> e04cf52b91d0a18e1348cabd0a6e4b8e462fcddb
